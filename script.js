c19293eed56c5cc4f94c14acd437e5875314375c
Santhoshb172 <90546392+Santhoshb172@users.noreply.github.com>
2021-09-25 10:11:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var req = new XMLHttpRequest();
+req.open('GET','https://restcountries.eu/rest/v2/all',true);
+req.send();
+req.onload=function(){
+    var result = JSON.parse(req.response);
+    for(let i=0;i<result.length;i++)
+    {
+        console.log(result);
+    }
+    
+}
